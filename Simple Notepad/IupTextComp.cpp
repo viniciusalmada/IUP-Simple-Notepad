dad31b7b27bc0b3bfe05071fc3ae45f3e5564b51
Vinicius Almada <viniciusalmada10@gmail.com>
2020-01-27 10:15:15 -0300

    New: class 'IupTextComp' created

---
+++ Simple Notepad/IupTextComp.cpp
@@ -0,0 +1,56 @@
+#include "IupTextComp.h"
+#include "Constants.h"
+
+void IupTextComp::isMultiline(bool value)
+{
+	if (value)
+		IupSetAttribute(iupText, Attr::MULTILINE, Val::YES);
+	else
+		IupSetAttribute(iupText, Attr::MULTILINE, Val::NO);
+}
+
+void IupTextComp::isExpanded(bool value)
+{
+	if (value)
+		IupSetAttribute(iupText, Attr::EXPAND, Val::YES);
+	else
+		IupSetAttribute(iupText, Attr::EXPAND, Val::NO);
+}
+
+void IupTextComp::setName(char* name)
+{
+	IupSetAttribute(iupText, Attr::NAME, name);
+}
+
+void IupTextComp::isDirty(bool value)
+{
+	if (value)
+		IupSetAttribute(iupText, Attr::DIRTY, Val::YES);
+	else
+		IupSetAttribute(iupText, Attr::DIRTY, Val::NO);
+}
+
+void IupTextComp::setFont(const char* font)
+{
+	IupSetStrAttribute(iupText, Attr::FONT, font);
+}
+
+char* IupTextComp::getName()
+{
+	return IupGetAttribute(iupText, Attr::NAME);
+}
+
+void IupTextComp::setCaretPositionCallback(Icallback call)
+{
+	IupSetCallback(iupText, Attr::CARET_CB, call);
+}
+
+void IupTextComp::setValueChangedCallback(Icallback call)
+{
+	IupSetCallback(iupText, Attr::VALUECHANGED_CB, call);
+}
+
+void IupTextComp::setDropFileCallback(Icallback call)
+{
+	IupSetCallback(iupText, Attr::DROPFILES_CB, call);
+}
